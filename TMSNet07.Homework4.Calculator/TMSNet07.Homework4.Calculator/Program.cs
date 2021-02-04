09c81d4f19b8d3e1b57f4d36fb784e71a1a46c69
Alesya <alesya.lis.1@mail.ru>
2021-02-04 23:59:11 +0300

    masterlevelcalculator

--- TMSNet07.Homework4.Calculator/TMSNet07.Homework4.Calculator/Program.cs
+++ TMSNet07.Homework4.Calculator/TMSNet07.Homework4.Calculator/Program.cs
@@ -13,31 +13,34 @@ namespace TMSNet07.Homework4.Calculator
             char repiat = 'y';
 
             while (Char.ToUpper(repiat) == 'Y')  
-                
             {
                 double firstValue;
-                double secondValue;
-                char oper;
+                double secondValue = 0;
+                string oper;
 
+                Console.WriteLine("Введите операцию: +, -, *, /, %, sqr, sqrt, pow");
+                oper = Console.ReadLine();
                 Console.WriteLine("Введите первое значение: ");
                 firstValue = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Введите операцию: +, -, *, /,% ");
-                oper = Convert.ToChar(Console.ReadLine());
-                Console.WriteLine("Введите второе значение");
-                secondValue = Convert.ToDouble(Console.ReadLine());
-
+                
+                if(oper != "sqr" & oper != "sqrt")
+                {
+                    Console.WriteLine("Введите второе значение");
+                    secondValue = Convert.ToDouble(Console.ReadLine());
+                }
+                
                 switch (oper)
                 {
-                    case '+':
+                    case "+":
                         Console.WriteLine("{0} + {1} = {2}", firstValue, secondValue, firstValue + secondValue);
                         break;
-                    case '-':
+                    case "-":
                         Console.WriteLine("{0} - {1} = {2}", firstValue, secondValue, firstValue - secondValue);
                         break;
-                    case '*':
+                    case "*":
                         Console.WriteLine("{0} * {1} = {2}", firstValue, secondValue, firstValue * secondValue);
                         break;
-                    case '/':
+                    case "/":
                         //check that the divisor is not 0
                         while (secondValue == 0)
                         {
@@ -46,13 +49,21 @@ namespace TMSNet07.Homework4.Calculator
                         }
                         Console.WriteLine("{0} / {1} = {2}", firstValue, secondValue, firstValue / secondValue);
                         break;
-                    case '%':
+                    case "%":
                         Console.WriteLine("{0} % {1} = {2}", firstValue, secondValue, firstValue % secondValue);
+                        break;
+                    case "sqr":
+                        Console.WriteLine("Результат возведения числа в степень = {0}", Math.Pow(firstValue, 2));
+                        break;
+                    case "sqrt":
+                        Console.WriteLine("Результат возведения квадратного кореня числа sqrt = {0}", Math.Sqrt(firstValue));
+                        break;
+                    case "pow":
+                        Console.WriteLine("Результат возведения числа в степень = {0}", Math.Pow(firstValue, secondValue));
                         break;
                     default:
                         Console.WriteLine("Ошибка");
                         break;
-
                 }
                 Console.WriteLine("Вы хотите продолжить операцию? Y/N ");
                 repiat = Convert.ToChar(Console.ReadLine());
