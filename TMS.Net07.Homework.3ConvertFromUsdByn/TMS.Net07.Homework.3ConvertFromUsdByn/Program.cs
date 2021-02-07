b0fa4aee9ea7f0fb715d2403824b0973e38c8b99
Alesya <alesya.lis.1@mail.ru>
2021-02-07 21:32:38 +0300

    task-3/convert-usd

--- TMS.Net07.Homework.3ConvertFromUsdByn/TMS.Net07.Homework.3ConvertFromUsdByn/Program.cs
+++ TMS.Net07.Homework.3ConvertFromUsdByn/TMS.Net07.Homework.3ConvertFromUsdByn/Program.cs
@@ -15,13 +15,9 @@ namespace TMS.Net07.Homework._3ConvertFromUsdByn
     {
         static void Main(string[] args)
         {
-           
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
             WebClient client = new WebClient();
-
-           Console.OutputEncoding = System.Text.Encoding.Unicode;
-
-
-
             string url = "https://www.nbrb.by/services/xmlexrates.aspx";
             var xml = client.DownloadString(url);
             XDocument xdoc = XDocument.Parse(xml);
@@ -36,22 +32,16 @@ namespace TMS.Net07.Homework._3ConvertFromUsdByn
             Console.WriteLine("Курс EUR " + eur);
             Console.WriteLine("Курс RUB " + rub / 100);
 
-
             do
             {
-
-
                 Console.WriteLine("Выберите валюту или EXIT для выхода : USD/EUR/BYN/RUB");
                 currency = Console.ReadLine();
 
                 if(currency.ToUpper() == "EXIT") { break; }
                 
-
                 Console.WriteLine("Введите сумму : ");
                 decimal value = Convert.ToDecimal(Console.ReadLine());
                 var cultureinfo = CultureInfo.GetCultureInfo("fr-FR");
-
-
 
                 switch (currency.ToUpper())
                 {
@@ -64,7 +54,6 @@ namespace TMS.Net07.Homework._3ConvertFromUsdByn
                         Console.WriteLine("BYN Br " + value * (decimal)eur);
                         Console.WriteLine("USD " + (value * (decimal)eur / (decimal)dollar).ToString("C4", new CultureInfo("en-US")));
                         Console.WriteLine("RUB " + "\u20BD " + (value * (decimal)eur / (decimal)rub * 100).ToString("F4"));
-
                         break;
                     case "BYN":
                         Console.WriteLine("USD " + (value / (decimal)dollar).ToString("C4", new CultureInfo("en-US")));
@@ -82,8 +71,6 @@ namespace TMS.Net07.Homework._3ConvertFromUsdByn
                 }
             }
             while (true);
-
-            
         }
     }
 }
